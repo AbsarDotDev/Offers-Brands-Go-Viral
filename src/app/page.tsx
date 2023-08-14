cb17dfdcfafeb1e0a6ecf561eee1b2876c0841b6
Absar Ali <absardotdev@Absars-MacBook-Pro.local>
2023-08-15 03:34:45 +0500

    CTA and hero section completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,58 @@
 import Image from 'next/image'
-import { Main } from './components/main'
+import { Hero } from '../components/hero'
+import { Cta } from '@/components/cta'
 
 export default function Home() {
+  const cta=[
+    {
+      heading:"You are struggling to create captivating Instagram posts that will engage your followers.",
+      paragraph:"Instagram is the most powerful platform for promoting your beauty brand and products to date. You may be an expert in your field, but if designing is not your cup of tea, you may end up designing a post that only you like. I understand that creating attractive Instagram posts that engage your audience can be challenging and you need a solution. Well, you are still able to design, all you need is an editable Instagram posts bundle to start with, which is a set of premade templates, specially designed for cosmetic brands & estheticians with all kinds of posts one should have on Instagram.",
+      image:"https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup.svg"
+    },
+    {
+      heading:"You need help creating consent forms that are compliant with all applicable laws.",
+      paragraph:"Consent forms are an important part of running a beauty business. They help to protect businesses from legal liability and ensure that clients are aware of their rights & give written consent about what they are up to. This is always a challenge to create your own consent forms from scratch in legal language as it is difficult and time-consuming, and you need to make sure to include all of the necessary information without making the form too long or complicated. Get all the consent forms for estheticians, simply add your brand name, and they're ready to sign and print!",
+      image:"https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup.svg"
+    },
+    {
+      heading:"You are having trouble designing packaging labels that reflect your brand identity.",
+      paragraph:"You are hiring an expensive designer from Fiverr & struggling to meet the requirements & not able to communicate the brand identity you have in your mind for your beauty package. Packaging labels are an essential part of branding. Good labels leave a lasting impression in a customer's mind, so they need to be minimal yet beautiful. Canva editable packaging labels with the brand's identity in terms of color scheme, typography, and overall design along with a print template for bulk printing can make your branding step hassle-free.",
+      image:"https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup.svg"
+    },
+    {
+      heading:"You are feeling overwhelmed by creating a cohesive business branding kit.",
+      paragraph:"I do understand why you might be feeling overwhelmed creating a perfect branding kit for your business. It can be a lot of work to put together all of the different pieces, and it can be hard to brand multiple social media in one style and theme. You need a branding kit in different sizes optimized for different platforms, ready to upload so you can start working on more important things in your business.",
+      image:"https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup.svg"
+    },
+
+    
+
+  ]
   return (
-  <main>
-    <Main/>
+  <main className='pl-5 pr-5'>
+    <Hero/>
+    <div className='text-center mt-20'>
+    <h1 className="mb-4  text-lg font-extrabold tracking-tight leading-none text-gray-900 md:text-lg lg:text-lg dark:text-white">Do you feel frustrated and overwhelmed that what actually should be the first step of launching a successful beauty business?</h1>
+    <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">You're passionate about beauty, but you procrastinate all day about where to start.
+</p>
+<h1 className="mb-4  text-lg font-extrabold tracking-tight leading-none text-bgv-primary md:text-lg lg:text-lg dark:text-white">DOES THIS SOUND LIKE YOU, RIGHT?
+</h1>
+    <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">I used to be just like you, always thinking, planning, and gathering information, but never achieving anything in the end.
+</p>
+    </div>
+    {
+  cta.map((cta, index) => {
+    return (
+      <Cta
+        heading={cta.heading}
+        paragraph={cta.paragraph}
+        image={cta.image}
+        index={index}
+      />
+    );
+  })
+}
+
     </main>
   )
 }
---
+++ src/components/cta.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image';
+import React from 'react';
+
+interface CtaProps {
+  heading: string;
+  paragraph: string;
+  image: string;
+  index: number;
+}
+
+export const Cta = ({ heading, paragraph, image, index }: CtaProps) => {
+  const isEven = index % 2 === 1;
+
+  return (
+    <section className={`bg-white dark:bg-gray-900 ${isEven ? 'flex-row-reverse' : ''}`}>
+      <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
+        {isEven ? (
+          <>
+            <div className="mt-4 md:mt-0 flex flex-col justify-center items-center">
+              <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">{heading}</h2>
+              <p className="mb-6 font-light text-gray-500 md:text-base dark:text-gray-400">{paragraph}</p>
+            </div>
+            <Image className="w-full" src={image} alt="dashboard image" width={1000} height={720} style={{ width: 'auto', height: 'auto' }} />
+          </>
+        ) : (
+          <>
+            <Image className="w-full" src={image} alt="dashboard image" width={1000} height={720} style={{ width: 'auto', height: 'auto' }} />
+            <div className="mt-4 md:mt-0 flex flex-col justify-center items-center">
+              <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">{heading}</h2>
+              <p className="mb-6 font-light text-gray-500 md:text-base dark:text-gray-400">{paragraph}</p>
+            </div>
+          </>
+        )}
+      </div>
+    </section>
+  );
+};
